30be81a0f3295ddfc3326671280ca685d7c560cc
Dorota Tomala <doforever@gmail.com>
2022-06-22 23:18:06 +0200

    Style contact

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,30 +1,27 @@
 import React from "react";
 
+//data
+const address = 'Gospodarstwo Szkółkarskie Jolanta i Kazimierz Tomala \n Buchałowice 64 24 - 150 \n Nałęczów woj.Lubelskie',
+  email = 'tomala.kazik@gmail.com',
+  visit = 'Zapraszam do odwiedzenia naszej szkółki od poniedziałku do soboty.';
+
+// markup
 const Contact = () => (
-  <section className="flex gap-4">
-    <div className="prose">
-      <h3>Adres</h3>
-      <address>
-        Gospodarstwo Szkółkarskie Jolanta i Kazimierz Tomala <br/>
-        Buchałowice 64 24-150 <br/>
-        Nałęczów woj. Lubelskie
-      </address>
-    </div>
-    <div className="prose">
+  <section className="flex flex-col md:flex-row gap-4 lg:gap-8 py-4 px-[5vw] justify-between">
+    <div className="prose flex-1 max-w-md">
       <h3>Kontakt</h3>
-      <address>
-        Gospodarstwo Szkółkarskie Jolanta i Kazimierz Tomala <br />
-        Buchałowice 64 24-150 <br />
-        Nałęczów woj. Lubelskie
+      <address className="not-italic">
+        <span>Email: </span>
+        <a href={`mailto: ${email}`}>{email}</a>
       </address>
     </div>
-    <div className="prose">
+    <div className="prose flex-1 max-w-md">
+      <h3>Adres</h3>
+      <address className="not-italic whitespace-pre-line">{address}</address>
+    </div>
+    <div className="prose flex-1 max-w-md">
       <h3>Odwiedziny</h3>
-      <address>
-        Gospodarstwo Szkółkarskie Jolanta i Kazimierz Tomala <br />
-        Buchałowice 64 24-150 <br />
-        Nałęczów woj. Lubelskie
-      </address>
+      <p>{visit}</p>
     </div>
   </section>
 );
