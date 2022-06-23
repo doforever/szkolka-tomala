de80fa381ac6b76a33f294c0c8c1d71fb7544977
Dorota Tomala <doforever@gmail.com>
2022-06-23 23:55:12 +0200

    Scroll gallery on mobile

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -32,13 +32,14 @@ const Gallery = () => {
 
   console.log('images', images);
 
-  return ( 
-    <section className="grid gap-4 lg:gap-8 py-4 px-[5vw] bg-green-800/5">
+  return (
+    <section className="grid grid-flow-col overflow-x-auto md:grid-flow-row gap-4 lg:gap-8 p-[5vw] bg-green-800/5 md:grid-cols-2 lg:grid-cols-3">
       {images.map((image: { node: { childImageSharp: { gatsbyImageData: any; }; base: string; }; }) => (
         <GatsbyImage
           key={image.node.base}
           image={image.node.childImageSharp.gatsbyImageData}
           alt={image.node.base.split(".")[0]}
+          className="w-[80vw]	md:w-auto"
         />
       ))}
     </section>
