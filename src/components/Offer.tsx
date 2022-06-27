c0893b6726d952ad20c83cf494bc9c292785e1fd
Dorota Tomala <doforever@gmail.com>
2022-06-27 10:33:53 +0200

    Style offer

--- src/components/Offer.tsx
+++ src/components/Offer.tsx
@@ -29,8 +29,8 @@ const headline = 'W sprzedaży m.in.',
 const Offer = () => {
   return (
     <section className="p-[5vw] bg-green-800/5">
-      <h2 className="text-xl font-semibold mb-2">{headline}</h2>
-      <ul className="grid gap-x-4 lg:gap-x-8 md:grid-cols-2 lg:grid-cols-3 justify-center">
+      <h2 className="text-xl font-semibold mb-4">{headline}</h2>
+      <ul className="grid gap-x-4 lg:gap-x-8 offer__columns"> 
         {varieties.map(({pl, lat}, index) => (<li key={index}>{pl} <i className="pl-1">{lat}</i></li>))}
       </ul>
     </section>
