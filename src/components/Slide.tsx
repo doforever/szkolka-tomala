c4628472fcce87cbad9678b207d1b8935c414128
Dorota Tomala <doforever@gmail.com>
2022-07-05 23:42:56 +0200

    Set title as image alt

--- src/components/Slide.tsx
+++ src/components/Slide.tsx
@@ -8,17 +8,22 @@ type SlideProps = {
   image: {node: { childImageSharp: any; base: string}},
 };
 
-const Slide = ({ image }: SlideProps): JSX.Element => (
-  <div className="relative">
-    <GatsbyImage
-      image={image.node.childImageSharp.gatsbyImageData}
-      alt={image.node.base.split(".")[0]}
-      className="w-[80vw]	md:w-auto overflow-hidden"
-    />
-    <h3 className="absolute bottom-0 capitalize text-center w-full p-2 bg-white/50 gallery__label">
-      {image.node.base.split(".")[0].replaceAll('_', ' ')}
-    </h3>
-  </div>
-);
+const Slide = ({ image }: SlideProps): JSX.Element => {
+  
+  const getTitle = (fileName: string) => fileName.split(".")[0].replaceAll('_', ' ');
+
+  return (
+    <div className="relative">
+      <GatsbyImage
+        image={image.node.childImageSharp.gatsbyImageData}
+        alt={getTitle(image.node.base)}
+        className="w-[80vw]	md:w-auto overflow-hidden"
+      />
+      <h3 className="absolute bottom-0 capitalize text-center w-full p-2 bg-white/50 gallery__label">
+        {getTitle(image.node.base)}
+      </h3>
+    </div>
+  );
+}
 
 export default Slide;
