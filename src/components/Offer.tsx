0debb5a5ba14acd51493c8601b1e7e42c2f2eb09
Dorota Tomala <doforever@gmail.com>
2022-12-18 22:31:52 +0100

    Update offer content

--- src/components/Offer.tsx
+++ src/components/Offer.tsx
@@ -26,11 +26,11 @@ const Offer = () => {
   return (
     <section className="p-[5vw] bg-green-800/5">
       <h2 className="text-xl font-semibold mb-4">{headline}</h2>
-      <ul className="grid gap-x-4 lg:gap-x-8 offer__columns">
+      <ul className="grid gap-x-4 lg:gap-x-8 offer__columns text-sm leading-loose">
         {trees.map(
           ({ pl, lac }: { pl: string; lac: string }, index: number) => (
             <li className="hidden lg:block" key={index}>
-              {pl} <i className="pl-1 text-black/60 text-sm">{lac}</i>
+              {pl} <i className="pl-1 text-black/60">{lac}</i>
             </li>
           )
         )}
