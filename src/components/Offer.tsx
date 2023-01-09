bb31731a871e03d18a1858bca89db3069ad46828
Dorota Tomala <doforever@gmail.com>
2023-01-09 22:10:59 +0100

    Sort trees on offer alphabetically

--- src/components/Offer.tsx
+++ src/components/Offer.tsx
@@ -16,9 +16,15 @@ const offerQuery = graphql`
 `;
 
 // markup
+type TreeType = { pl: string; lac: string };
+
 const Offer = () => {
   const {
     allOfferCsv: { nodes: trees },
+  }: {
+    allOfferCsv: {
+      nodes: [TreeType];
+    };
   } = useStaticQuery(offerQuery);
 
   // console.log('trees', trees);
@@ -27,13 +33,13 @@ const Offer = () => {
     <section className="p-[5vw] bg-green-800/5">
       <h2 className="text-xl font-semibold mb-4">{headline}</h2>
       <ul className="grid gap-x-4 lg:gap-x-8 offer__columns text-sm leading-loose">
-        {trees.map(
-          ({ pl, lac }: { pl: string; lac: string }, index: number) => (
+        {trees
+          .sort((a, b) => a.pl.localeCompare(b.pl))
+          .map(({ pl, lac }, index: number) => (
             <li className="hidden lg:block" key={index}>
               {pl} <i className="pl-1 text-black/60">{lac}</i>
             </li>
-          )
-        )}
+          ))}
       </ul>
     </section>
   );
